a1d4af5496681bc48f990dac95fa2992b43d8da6
Paulo Nepotti <p_nepotti@hotmail.com>
2026-08-25 21:36:42 -0300

    feat: creación atómica en una sola petición POST /personas-ep

--- src/services/paciente.service.js
+++ src/services/paciente.service.js
@@ -5,69 +5,45 @@ const pacientes = {
   async guardarPaciente(data) {
     const valorNumerico = (valor) => (valor === '' || valor === undefined ? null : Number(valor));
 
-    let dataDireR = {
-      calle: data.calleR,
-      departamento: data.departamentoR || null,
-      numero: valorNumerico(data.numeroR),
-      piso: valorNumerico(data.pisoR),
-      idlocalidad: valorNumerico(data.localidadR),
-    };
-    let dataDireEP = {
-      calle: data.calleEP,
-      departamento: data.departamentoEP || null,
-      numero: valorNumerico(data.numeroEP),
-      piso: valorNumerico(data.pisoEP),
-      idlocalidad: valorNumerico(data.localidadEP),
-    };
-
-    let direccionR = await http.post(`/direcciones`, dataDireR);
-    let direccionEP = await http.post(`/direcciones`, dataDireEP);
-
-    let dataPersR = {
-      nombre: data.nombreR,
-      apellido: data.apellidoR,
-      telefono: data.telefonoR,
-      iddireccion: direccionR.data.iddireccion,
-      borrado: 0,
-      espaciente: 0,
-    };
-    let dataPersEP = {
+    const payload = {
       nombre: data.nombreEP,
       apellido: data.apellidoEP,
       telefono: data.telefonoEP,
-      iddireccion: direccionEP.data.iddireccion,
-      borrado: 0,
-      espaciente: 1,
-    };
-
-    let personaR = await http.post(`/personas`, dataPersR);
-    let personaEP = await http.post(`/personas`, dataPersEP);
-
-    let dataPaciente = {
-      nombre: data.nombreEP,
-      apellido: data.apellidoEP,
-      telefono: data.telefonoEP,
-      borrado: 0,
-      espaciente: 1,
-
+      sexo: data.sexoEP,
+      fechanacimiento: data.nacimientoEP,
       activataller: 1,
       escolaridadcompleta: 0,
       fechainicio: new Date(),
-      fechanacimiento: data.nacimientoEP,
       maximaescolaridadalcanzada: data.escolaridadEP,
-      sexo: data.sexoEP,
       tieneacompanante: data.tieneAcompananteEP ? 1 : 0,
       tienecuidador: data.tieneCuidadorEP ? 1 : 0,
       vivesolo: data.viveSoloEP ? 1 : 0,
       ocupacionprevia: data.ocupacionPEP,
       ocupacionactual: data.ocupacionAEP,
-
-      idpersona: personaEP.data.idpersona,
-      idreferente: personaR.data.idpersona,
+      direccion: {
+        calle: data.calleEP,
+        departamento: data.departamentoEP || null,
+        numero: valorNumerico(data.numeroEP),
+        piso: valorNumerico(data.pisoEP),
+        idlocalidad: valorNumerico(data.localidadEP),
+      },
+      referente: {
+        nombre: data.nombreR,
+        apellido: data.apellidoR,
+        telefono: data.telefonoR,
+        sexo: data.sexoR,
+        fechanacimiento: data.nacimientoR,
+        direccion: {
+          calle: data.calleR,
+          departamento: data.departamentoR || null,
+          numero: valorNumerico(data.numeroR),
+          piso: valorNumerico(data.pisoR),
+          idlocalidad: valorNumerico(data.localidadR),
+        },
+      },
     };
 
-    let paciente = await http.post(`/personas-ep`, dataPaciente);
-
+    const paciente = await http.post(`/personas-ep`, payload);
     return paciente.data;
   },
 
